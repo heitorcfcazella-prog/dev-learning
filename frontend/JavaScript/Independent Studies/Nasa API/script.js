2a47c85c23ac424ce219c5ed123fdd0fed4bd118
Heitor César <heitorcfcazella@gmail.com>
2026-02-22 19:26:25 -0300

    study: image of the day using NASA API

---
+++ frontend/JavaScript/Independent Studies/Nasa API/script.js
@@ -0,0 +1,25 @@
+async function fetchData() {
+  try{
+    const year = document.getElementById("year").value;
+    const month = document.getElementById("month").value.padStart(2, "0");;
+    const day = document.getElementById("day").value.padStart(2, "0");;
+    const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=98btlocCYGd20Zhf3DMLKrMpC31ETGxqXBAFXLZZ&date=${year}-${month}-${day}`)
+
+    if(!response.ok){
+      throw new Error("Não foi encontarda");
+    }
+
+    const data = await response.json();
+    const imgElement = document.getElementById("image");
+
+    if (data.media_type === "image") {
+      imgElement.src = data.url;
+      imgElement.style.display = "block";
+    } else {
+      alert("A data selecionada não contém uma imagem.");
+    }
+  }
+  catch(error){
+    console.log(error);
+  }
+}
