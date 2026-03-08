2afa883a91f78f5e005042402518ac8d79e0d79b
Heitor César <heitorcfcazella@gmail.com>
2026-03-08 17:59:45 -0300

    study: fundamentals of ES module using node.js

---
+++ backend/node.js/travesy media/index.js
@@ -0,0 +1,14 @@
+import getPosts, { getPostsLenght } from './postControler.js';
+
+console.log(getPosts());
+
+console.log(`Quantidades de  posts: ${getPostsLenght()}`);
+
+
+/*
+const {generateRandomNumber, celciusToFahrenheit} = require('./utils.js');
+
+console.log(`Numero Aleatório: ${generateRandomNumber()}`);
+
+console.log(`Celcius para Fahrenhiet: ${celciusToFahrenheit(generateRandomNumber())}`);
+*/
---
+++ backend/node.js/travesy media/postControler.js
@@ -0,0 +1,10 @@
+const posts = [
+  {id: 1, title: 'Post Um'},
+  {id: 2, title: 'Dois'}
+];
+
+const getPosts = () => posts;
+
+export const getPostsLenght = () => posts.length;
+
+export default getPosts;
