1c2d72619e06c11d51a60b05198e42cf38b73a3f
Heitor César <heitorcfcazella@gmail.com>
2026-03-22 21:08:55 -0300

    study: Java Statements base

---
+++ backend/Java/Syntax/Statements/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Java is fun!");
+
+        System.out.println("Hello World!");
+        System.out.println("Have a good day!");
+        System.out.println("Learning Java is fun!");
+    }
+}
