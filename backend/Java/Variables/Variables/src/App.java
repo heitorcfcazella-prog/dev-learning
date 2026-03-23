544c7da7774f9620b93828c286bcde041896ef64
Heitor César <heitorcfcazella@gmail.com>
2026-03-22 21:25:31 -0300

    study: Java variables

---
+++ backend/Java/Variables/Variables/src/App.java
@@ -0,0 +1,24 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        /*
+        String - Texto
+        int - Número Inteiro
+        float - Número com decimais
+        char - Um caractere
+        boolean - True ou False
+        */
+
+       String name = "John";
+       System.out.println(name);
+       int num = 15;
+       num = 20;
+       System.out.println(num);
+
+       // Outros tipos
+       int num2 = 5;
+       float floatNum = 5.99f; // o 'f' no final define coo float, por que 5.99 é naturalmente double
+       char letter = 'A';
+       boolean bool = true;
+       String text = "Hello";
+    }
+}
