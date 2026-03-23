37f24d041f170c500a15b1fb649f025bbf1690fb
Heitor César <heitorcfcazella@gmail.com>
2026-03-22 21:12:15 -0300

    study: Java comments

---
+++ backend/Java/Syntax/Statements/src/comments.java
@@ -0,0 +1,9 @@
+public class comments {
+  public static void main(String[] args) {
+    System.out.println("Hello World!"); // Isso mostra 'Hello World!' no console
+    System.out.println("Another Comment");
+    /*
+    Comentário em multi-linhas
+    */
+  }
+}
