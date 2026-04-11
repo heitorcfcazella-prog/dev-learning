5baf0ac57592df0202d639376b84d4c79a4234fc
Heitor César <heitorcfcazella@gmail.com>
2026-04-10 21:59:08 -0300

    style: shopping page style and text

--- frontend/HTML-CSS/Independent Studies/Shopping page/script.js
+++ frontend/HTML-CSS/Independent Studies/Shopping page/script.js
@@ -4,11 +4,11 @@ const container = document.getElementById('lista-produtos');
 
 function carregarProdutos(){
   container.innerHTML = produtos.map(produto => `
-    <div class=produto>
+    <div class="produto">
       <img src="${produto.img}" alt="${produto.nome}">
       <h3>${produto.nome}</h3>
       <p>Preço: R$${produto.preco.toFixed(2)}</p>
-      <button>Coloque no Carinho</button>
+      <button class="botao">Coloque no Carinho</button>
     </div>
     `).join('');
 }
